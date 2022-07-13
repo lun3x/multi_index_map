49da4acf1c4c96877b022cb96deec46437279361
Louis Wyborn <louiswyborn@gmail.com>
2022-07-13 16:53:53 +0200

    More test refactoring

--- tests/ordered_unique.rs
+++ tests/ordered_unique.rs
@@ -16,13 +16,13 @@ fn test_insert_and_get() {
     let mut map = MultiIndexTestElementMap::default();
     let elem1 = TestElement {
         field1: TestNonPrimitiveType(42),
-        field2: "ElementOneFieldTwo".to_string(),
+        field2: "ElementOne".to_string(),
     };
     map.insert(elem1);
 
     let elem1_ref = map.get_by_field1(&TestNonPrimitiveType(42)).unwrap();
     assert_eq!(elem1_ref.field1.0, 42);
-    assert_eq!(elem1_ref.field2, "ElementOneFieldTwo");
+    assert_eq!(elem1_ref.field2, "ElementOne");
     assert_eq!(map.len(), 1);
 }
 
@@ -31,23 +31,23 @@ fn test_insert_and_remove_by_field1() {
     let mut map = MultiIndexTestElementMap::default();
     let elem1 = TestElement {
         field1: TestNonPrimitiveType(42),
-        field2: "ElementOneFieldTwo".to_string(),
+        field2: "ElementOne".to_string(),
     };
     let elem2 = TestElement {
         field1: TestNonPrimitiveType(43),
-        field2: "ElementTwoFieldTwo".to_string(),
+        field2: "ElementTwo".to_string(),
     };
     map.insert(elem1);
     map.insert(elem2);
 
     let elem1 = map.remove_by_field1(&TestNonPrimitiveType(42)).unwrap();
     assert_eq!(elem1.field1.0, 42);
-    assert_eq!(elem1.field2, "ElementOneFieldTwo");
+    assert_eq!(elem1.field2, "ElementOne");
     assert_eq!(map.len(), 1);
 
     let elem2 = map.remove_by_field1(&TestNonPrimitiveType(43)).unwrap();
     assert_eq!(elem2.field1.0, 43);
-    assert_eq!(elem2.field2, "ElementTwoFieldTwo");
+    assert_eq!(elem2.field2, "ElementTwo");
     assert!(map.is_empty());
 }
 
