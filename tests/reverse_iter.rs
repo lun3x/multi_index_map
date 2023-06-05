ffe3864cac075d4d04b1cfe7cc310dfa5160e54d
wyjin <22771792+wyjin@users.noreply.github.com>
2023-06-05 16:32:03 -0500

    fixed rev_iter tests

--- tests/reverse_iter.rs
+++ tests/reverse_iter.rs
@@ -1,5 +1,5 @@
 use multi_index_map::MultiIndexMap;
-#[derive(MultiIndexMap, Clone)]
+#[derive(MultiIndexMap, Clone, PartialEq, Debug)]
 struct TestElement {
     #[multi_index(ordered_non_unique)]
     field1: usize,
@@ -22,14 +22,14 @@ fn test_non_unique_reverse_iter() {
         assert!(elem.field1 <= prev);
         prev = elem.field1;
     }
-    
+
     let mut prev = 20; 
     for (_i, elem) in s.iter_by_field2().rev().enumerate() {
         assert!(elem.field2 <= prev);
         prev = elem.field2;
     }
 
-    s.modify_by_field2(&2, |e| {e.field1 = 2});
+    s.modify_by_field2(&12, |e| {e.field1 = 2});
     s.modify_by_field2(&3, |e| {e.field1 = 2});
     let mut prev = 6;
     for (_i, elem) in s.iter_by_field1().rev().enumerate() {
@@ -42,4 +42,14 @@ fn test_non_unique_reverse_iter() {
         assert!(elem.field2 <= prev);
         prev = elem.field2;
     }
+
+    let mut it = s.iter_by_field2();
+    assert_eq!(it.next_back().unwrap().field2, 12);
+    assert_eq!(it.next_back().unwrap().field2, 11);
+    assert_eq!(it.next_back().unwrap().field2, 10);
+    assert_eq!(it.next_back().unwrap().field2, 5);
+    assert_eq!(it.next_back().unwrap().field2, 4);
+    assert_eq!(it.next_back().unwrap().field2, 3);
+    assert_eq!(it.next_back(), None);
+
 }
